45585b0e5798178b1f38f8372adf087bf15b38db
Eliot <eliotpitman@gmail.com>
2022-08-21 00:41:06 -0500

    adds safeareaview

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Button title="Press me" disabled onPress={() => Alert.alert("Cannot press this one")} />
       <Text>Hello world! Welcome to Chronicle</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gray',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
